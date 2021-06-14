212f64505b4aafabaad5cac961be07729a611910
Kaushik Shamantha <kaushikshamantha@gmail.com>
2021-06-14 23:06:41 +0530

    registration API

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   namespace :api do
-    resources :users
+    resources :users, only: [:create]
     post 'login', to: 'sessions#create'
     get 'logout', to: 'sessions#destroy'
     
