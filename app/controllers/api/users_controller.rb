3a6e42c7928794cbfc24ab8bc0f06835769f59b7
Kaushik Shamantha <kaushikshamantha@gmail.com>
2021-06-18 10:38:57 +0530

    add user errors to API

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,20 +1,18 @@
 class Api::UsersController < ApplicationController
   def create
-    begin
-      user = User.new(user_params)
-      user.save
-
+    user = User.new(user_params)
+     if user.save
       render json: {
         name: user.name,
-        email: user.email
+        email: user.email,
+        message: "User has created been successfully."
       },
-      status: :created
-
-    rescue ActiveRecord::ActiveRecordError => are
-
-    rescue Exception
-      # handle everything else
-      raise
+      status: :create
+    else
+      render json: {
+        errors: user.errors
+      },
+      status: :bad_request
     end
   end
 
